ceb308b466ed4c563ef34171a9557507f6449966
anh-zero <anh.gm.gz@gmail.com>
2024-11-04 02:26:20 +0700

    Login Register Nodejs AJAX

---
+++ src/public/js/login.js
@@ -0,0 +1,23 @@
+
+function handleLoginBtn() {
+    $("#button_login").on("click", function (event) {
+        event.preventDefault();
+        let email = $("#email").val();
+        let password = $("#password-input").val();
+
+        $.ajax({
+            url: `${window.location.origin}/login`,
+            method: "POST",
+            data: { email: email, password: password },
+            success: function (data) {
+                window.location.href = "/";
+            },
+            error: function (err) {
+                alert("Your email or password entered is incorrect. Please try again!");
+            }
+        })
+    });
+}
+$(document).ready(function () {
+    handleLoginBtn();
+});
